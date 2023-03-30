c95e7e6133e79d431871e72c986c904c4960b553
Krystal <1760739491@qq.com>
2023-03-30 22:18:47 +0800

    Initial commit

---
+++ src/main/java/com/example/jpetstore_sb/JpetstoreSbApplication.java
@@ -0,0 +1,13 @@
+package com.example.jpetstore_sb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpetstoreSbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpetstoreSbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jpetstore_sb/JpetstoreSbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpetstore_sb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpetstoreSbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
